408c3f54d9022e2052dbda0de7b0d9e9a5748ba6
Jakub Nowak <nowakjakub095@gmail.com>
2020-01-31 13:13:05 +0100

    summing repaired

--- src/main/java/MunchkinCalc/Munchkin/App.java
+++ src/main/java/MunchkinCalc/Munchkin/App.java
@@ -21,58 +21,56 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import MunchkinCalc.Munchkin.Player;
 
-public class App
-{
-	String[] nr = {"2","3","4","5","6","7","8","9","10","11","12"};
+public class App {
+	String[] nr = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
 	ArrayList<String> playersNames = new ArrayList<String>();
 	ArrayList<Integer> playersStrength = new ArrayList<Integer>();
+	ArrayList<Object> playerObject = new ArrayList<Object>();
 	JFrame mainFrame = new JFrame();
 	JPanel mainPanel = new JPanel();
-	
-	
-	App()
-	{
+
+	App() {
 		initComponets();
 	}
-	
-	void initComponets()
-	{
+
+	void initComponets() {
 		/*
 		 * Asking for confirmation of exiting the program
 		 */
-		mainFrame.addWindowListener(new WindowAdapter()
-				{
+		mainFrame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				int i = JOptionPane.showConfirmDialog(mainFrame, "Do you want to exit?");
-				if(i == 0)
-				{
+				if (i == 0) {
 					System.exit(0);
 				}
 			}
 
-				});
+		});
 		/*
 		 * Setting the bounds and properties of mainFrame
 		 */
-		mainFrame.setBounds(Toolkit.getDefaultToolkit().getScreenSize().width/3, Toolkit.getDefaultToolkit().getScreenSize().height/3,
-				Toolkit.getDefaultToolkit().getScreenSize().width/2, Toolkit.getDefaultToolkit().getScreenSize().height/2);
+		mainFrame.setBounds(Toolkit.getDefaultToolkit().getScreenSize().width / 3,
+				Toolkit.getDefaultToolkit().getScreenSize().height / 3,
+				Toolkit.getDefaultToolkit().getScreenSize().width / 2,
+				Toolkit.getDefaultToolkit().getScreenSize().height / 2);
 		mainFrame.setVisible(true);
 		mainFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		mainFrame.add(mainPanel);
-		mainPanel.setLayout(new GridLayout(0,6));
-		String playersNumber = (String) JOptionPane.showInputDialog(mainFrame,"Enter number of players","Number of Players",
-				JOptionPane.QUESTION_MESSAGE, null,nr,nr[0]);
+		mainPanel.setLayout(new GridLayout(0, 6));
+		String playersNumber = (String) JOptionPane.showInputDialog(mainFrame, "Enter number of players",
+				"Number of Players", JOptionPane.QUESTION_MESSAGE, null, nr, nr[0]);
 		int playersNr = Integer.parseInt(playersNumber);
-		
+
 		/*
 		 * creating new players
 		 */
-		Player player = new Player(mainFrame, mainPanel, playersNr);
+		Player player = new Player(mainFrame, mainPanel, playersNr, playersStrength);
+//		System.out.println(playersStrength.get(player.getClass().hashCode()));
+		
 	}
-	
-	public static void main( String[] args )
-    {
-    	new App();
-    }
+
+	public static void main(String[] args) {
+		new App();
+	}
 
 }
--- src/main/java/MunchkinCalc/Munchkin/Player.java
+++ src/main/java/MunchkinCalc/Munchkin/Player.java
@@ -16,72 +16,79 @@ import javax.swing.event.ChangeListener;
 /*
  * Class to make players and add them to Frame
  */
-public class Player{
+public class Player {
 	ArrayList<String> playersNames = new ArrayList<String>();
 	ArrayList<Integer> playersStrength = new ArrayList<Integer>();
 	int gearValue = 0;
 	int lvlValue = 0;
-	
-	Player(JFrame mainFrame, JPanel mainPanel, int playersNr){
-		for(int i=0; i<=playersNr; i++)
-		{
+	int summLvlValue = gearValue + lvlValue;
+
+	Player(JFrame mainFrame, JPanel mainPanel, int playersNr, final ArrayList<Integer> playersStrength) {
+//		playersStrength.add(Player.class.hashCode(), summLvlValue);
+		for (int i = 0; i <= playersNr; i++) {
 			/*
-			 * Creating option panes to insert
-			 * players names and adding them to variable
+			 * Creating option panes to insert players names and adding them to variable
 			 */
 			JTextField xField = new JTextField(5);
 			JPanel myPanel = new JPanel();
-		    myPanel.add(new JLabel("enter name of player "+i));
-		    myPanel.add(xField);
-		    myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-		   
-		      int result = JOptionPane.showConfirmDialog(null, myPanel, 
-		               "Please enter players name", JOptionPane.OK_CANCEL_OPTION);
-		      if (result == JOptionPane.OK_OPTION) {
-		         System.out.println("name: " + xField.getText());
-		         playersNames.add(i,xField.getText().toString());
-		         	      }
-		      else if(result == JOptionPane.CANCEL_OPTION) {
-		    	  break;
-		      }
-		      /*
-		       * Adding labels and spinners to mainPanel and
-		       * creating change listeners for spinners
-		       */
-		      JLabel name = new JLabel(playersNames.get(i));
-			  JLabel lvl = new JLabel("Lvl");
-			  final JSpinner lvlSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 10, 1));
-			      	lvlSpinner.addChangeListener(new ChangeListener() {
-						public void stateChanged(ChangeEvent e) {
-							int lvlValue = (Integer) lvlSpinner.getValue();
-							System.out.println(lvlValue);
-						}
-			      	});
-			      	
-			   JLabel gear = new JLabel("Gear lvl");
-			   final JSpinner gearSpinner = new JSpinner(new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1));
-			      	gearSpinner.addChangeListener(new ChangeListener() {
-						public void stateChanged(ChangeEvent e) {
-							int gearValue = (Integer) gearSpinner.getValue();
-							System.out.println(gearValue);
-						}
-			      	});
-			    int summLvlValue = gearValue + lvlValue;
-			    playersStrength.add(i, summLvlValue);
-			    JLabel summLvl = new JLabel("Strenght: " + summLvlValue);
-			    mainPanel.add(name);
-			    mainPanel.add(lvl);
-			    mainPanel.add(lvlSpinner);
-			    mainPanel.add(gear);
-			    mainPanel.add(gearSpinner);
-			    mainPanel.add(summLvl);
-				/*
-				 * Refresing mainFrame
-				 */
-				mainFrame.revalidate();
-				mainFrame.repaint();
+			final JLabel summLvl = new JLabel("Strenght: " + summLvlValue);
+
+			myPanel.add(new JLabel("enter name of player " + i));
+			myPanel.add(xField);
+			myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+
+			int result = JOptionPane.showConfirmDialog(null, myPanel, "Please enter players name",
+					JOptionPane.OK_CANCEL_OPTION);
+			if (result == JOptionPane.OK_OPTION) {
+				System.out.println("name: " + xField.getText());
+				playersNames.add(i, xField.getText().toString());
+			} else if (result == JOptionPane.CANCEL_OPTION) {
+				break;
+			}
+			/*
+			 * Adding labels and spinners to mainPanel and creating change listeners for
+			 * spinners
+			 */
+			JLabel name = new JLabel(playersNames.get(i));
+			JLabel lvl = new JLabel("Lvl");
+			final JSpinner lvlSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 10, 1));
+			lvlSpinner.addChangeListener(new ChangeListener() {
+				public void stateChanged(ChangeEvent e) {
+					lvlValue = (Integer) lvlSpinner.getValue();
+					System.out.println(lvlValue);
+					summLvlValue = gearValue + lvlValue;
+					summLvl.setText("Strenght: " + summLvlValue);
+//					playersStrength.set(Player.class.hashCode(), summLvlValue);
+				}
+			});
+
+			JLabel gear = new JLabel("Gear lvl");
+			final JSpinner gearSpinner = new JSpinner(new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1));
+			gearSpinner.addChangeListener(new ChangeListener() {
+				public void stateChanged(ChangeEvent e) {
+					gearValue = (Integer) gearSpinner.getValue();
+					System.out.println(gearValue);
+					summLvlValue = gearValue + lvlValue;
+					summLvl.setText("Strenght: " + summLvlValue);
+//					playersStrength.set(Player.class.hashCode(), summLvlValue);
+				}
+			});
+
+			playersStrength.add(i, summLvlValue);
+			mainPanel.add(name);
+			mainPanel.add(lvl);
+			mainPanel.add(lvlSpinner);
+			mainPanel.add(gear);
+			mainPanel.add(gearSpinner);
+			mainPanel.add(summLvl);
+			/*
+			 * Refresing mainFrame
+			 */
+			mainFrame.revalidate();
+			mainFrame.repaint();
+			System.out.println();
 		}
-	
+
 	}
 
 }
